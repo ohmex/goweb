ed9ccdab392c9a17989f6d200f9ac137c74dc4b9
Sachin <sachin.sharma4@publicissapient.com>
2024-05-25 16:04:48 +0530

    query optimisations & json fields fix

--- interceptor/middlewares.go
+++ interceptor/middlewares.go
@@ -15,29 +15,6 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func CasbinAuthorization(server *server.Server) echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			// Get tenant from header identified by UUID
-			tenant := c.Request().Header.Get("tenant")
-
-			// Get user name as UUID
-			user := c.Get("user").(*models.User).UUID.String()
-
-			// Check, user though mabe assiciated with a tenant in DB
-			// Does he has casbin domain assigned to him or not
-			domains, _ := server.Casbin.GetDomainsForUser(user)
-			ok := util.Contains[string](domains, tenant)
-
-			if ok {
-				return next(c)
-			}
-
-			return api.WebResponse(c, http.StatusForbidden, api.CASBIN_UNAUTHORIZED())
-		}
-	}
-}
-
 // Middleware for additional steps:
 // 1. Check the token info exists in Redis
 // 2. Check the user exists in DB
@@ -70,6 +47,29 @@ func JwtAuthorization(server *server.Server) echo.MiddlewareFunc {
 			}()
 
 			return next(c)
+		}
+	}
+}
+
+func CasbinAuthorization(server *server.Server) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			// Get tenant from header identified by UUID
+			tenant := c.Request().Header.Get("tenant")
+
+			// Get user name as UUID
+			user := c.Get("user").(*models.User).UUID.String()
+
+			// Check, user though mabe assiciated with a tenant in DB
+			// Does he has casbin domain assigned to him or not
+			domains, _ := server.Casbin.GetDomainsForUser(user)
+			ok := util.Contains[string](domains, tenant)
+
+			if ok {
+				return next(c)
+			}
+
+			return api.WebResponse(c, http.StatusForbidden, api.CASBIN_UNAUTHORIZED())
 		}
 	}
 }
