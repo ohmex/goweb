767d48e3917b31dfea506e65b921db807986ee17
Sachin <sachin.sharma4@publicissapient.com>
2025-07-04 01:31:03 +0530

    updated code

--- api/response.go
+++ api/response.go
@@ -19,7 +19,7 @@ func WebResponse(c echo.Context, statusCode int, data interface{}) error {
 	return c.JSON(statusCode, data)
 }
 
-// Response codes
+// Error codes
 const (
 	CodeFieldValidationError   = 100000
 	CodeInvalidCredentials     = 100001
@@ -33,158 +33,109 @@ const (
 	CodeCasbinUnauthorized     = 100011
 	CodeResourceCreationFailed = 100012
 	CodeResourceExists         = 100013
+)
 
+// Status codes
+const (
 	CodeStatusOK        = 200000
 	CodeResourceCreated = 200001
 	CodeResourceDeleted = 200002
 	CodeUserLoggedOut   = 200003
 )
 
-// newResponse is a helper to create a Response.
-func newResponse(code uint64, err, msg string) Response {
-	return Response{Code: code, Err: err, Msg: msg}
+// responseTemplate is a helper to create a Response with flexible error/message placement.
+func responseTemplate(code uint64, defaultMsg string, isError bool, s ...string) Response {
+	msg := defaultMsg
+	if len(s) > 0 {
+		msg = s[0]
+	}
+	if isError {
+		return newResponse(code, msg, "")
+	}
+	return newResponse(code, "", msg)
 }
 
 // FIELD_VALIDATION_ERROR returns a response for field validation errors.
 func FIELD_VALIDATION_ERROR(s ...string) Response {
-	data := "Required fields are empty or not valid"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeFieldValidationError, data, "")
+	return responseTemplate(CodeFieldValidationError, "Required fields are empty or not valid", true, s...)
 }
 
 // INVALID_CREDENTIALS returns a response for invalid credentials.
 func INVALID_CREDENTIALS(s ...string) Response {
-	data := "Invalid credentials"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeInvalidCredentials, data, "")
+	return responseTemplate(CodeInvalidCredentials, "Invalid credentials", true, s...)
 }
 
 // INVALID_SIGNING_METHOD returns a response for invalid signing method.
 func INVALID_SIGNING_METHOD(s ...string) Response {
-	data := "Invalid signing method"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeInvalidSigningMethod, data, "")
+	return responseTemplate(CodeInvalidSigningMethod, "Invalid signing method", true, s...)
 }
 
 // INVALID_TOKEN returns a response for invalid token.
 func INVALID_TOKEN(s ...string) Response {
-	data := "Invalid token"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeInvalidToken, data, "")
+	return responseTemplate(CodeInvalidToken, "Invalid token", true, s...)
 }
 
 // USER_NOT_FOUND returns a response for user not found.
 func USER_NOT_FOUND(s ...string) Response {
-	data := "User not found"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeUserNotFound, data, "")
+	return responseTemplate(CodeUserNotFound, "User not found", true, s...)
 }
 
 // TOKEN_EXPIRED returns a response for expired token.
 func TOKEN_EXPIRED(s ...string) Response {
-	data := "Token expired"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeTokenExpired, data, "")
+	return responseTemplate(CodeTokenExpired, "Token expired", true, s...)
 }
 
 // USER_EXISTS returns a response for user already exists.
 func USER_EXISTS(s ...string) Response {
-	data := "User already exists"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeUserExists, data, "")
+	return responseTemplate(CodeUserExists, "User already exists", true, s...)
 }
 
 // INTERNAL_SERVICE_ERROR returns a response for internal service errors.
 func INTERNAL_SERVICE_ERROR(s ...string) Response {
-	data := "Internal service error"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeInternalServiceError, data, "")
+	return responseTemplate(CodeInternalServiceError, "Internal service error", true, s...)
 }
 
 // RESOURCE_NOT_FOUND returns a response for resource not found.
 func RESOURCE_NOT_FOUND(s ...string) Response {
-	data := "Resource not found"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeResourceNotFound, data, "")
+	return responseTemplate(CodeResourceNotFound, "Resource not found", true, s...)
 }
 
 // CASBIN_UNAUTHORIZED returns a response for authorization failures.
 func CASBIN_UNAUTHORIZED(s ...string) Response {
-	data := "Access denied - resource authorization failed"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeCasbinUnauthorized, data, "")
+	return responseTemplate(CodeCasbinUnauthorized, "Access denied - resource authorization failed", true, s...)
 }
 
 // RESOURCE_CREATION_FAILED returns a response for resource creation failures.
 func RESOURCE_CREATION_FAILED(s ...string) Response {
-	data := "Resource creation failed"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeResourceCreationFailed, data, "")
+	return responseTemplate(CodeResourceCreationFailed, "Resource creation failed", true, s...)
 }
 
 // RESOURCE_EXISTS returns a response for resource already exists.
 func RESOURCE_EXISTS(s ...string) Response {
-	data := "Resource already exists"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeResourceExists, "", data)
+	return responseTemplate(CodeResourceExists, "Resource already exists", false, s...)
 }
 
 // STATUS_OK returns a response for successful operations.
 func STATUS_OK(s ...string) Response {
-	data := "Ok"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeStatusOK, "", data)
+	return responseTemplate(CodeStatusOK, "Ok", false, s...)
 }
 
 // RESOURCE_CREATED returns a response for successful resource creation.
 func RESOURCE_CREATED(s ...string) Response {
-	data := "Resource successfully created"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeResourceCreated, "", data)
+	return responseTemplate(CodeResourceCreated, "Resource successfully created", false, s...)
 }
 
 // RESOURCE_DELETED returns a response for successful resource deletion.
 func RESOURCE_DELETED(s ...string) Response {
-	data := "Resource deleted"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeResourceDeleted, "", data)
+	return responseTemplate(CodeResourceDeleted, "Resource deleted", false, s...)
 }
 
 // USER_LOGGED_OUT returns a response for successful user logout.
 func USER_LOGGED_OUT(s ...string) Response {
-	data := "User logged out"
-	if len(s) > 0 {
-		data = s[0]
-	}
-	return newResponse(CodeUserLoggedOut, "", data)
+	return responseTemplate(CodeUserLoggedOut, "User logged out", false, s...)
+}
+
+// newResponse is a helper to create a Response.
+func newResponse(code uint64, err, msg string) Response {
+	return Response{Code: code, Err: err, Msg: msg}
 }
