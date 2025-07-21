85c9516e8df9647d1e7cd27ed5ba6620d6271c3d
Sachin <sachin.sharma4@publicissapient.com>
2025-07-22 01:35:52 +0530

    added postgresql too driver options

--- db/connection.go
+++ db/connection.go
@@ -9,17 +9,15 @@ import (
 
 	"github.com/redis/go-redis/v9"
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
 func InitDB(cfg *config.Config) *gorm.DB {
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		cfg.DB.User,
-		cfg.DB.Password,
-		cfg.DB.Host,
-		cfg.DB.Port,
-		cfg.DB.Name)
+	var db *gorm.DB
+	var err error
+	var dataSourceName string
 
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
@@ -32,7 +30,27 @@ func InitDB(cfg *config.Config) *gorm.DB {
 		},
 	)
 
-	db, err := gorm.Open(mysql.Open(dataSourceName), &gorm.Config{Logger: newLogger})
+	switch cfg.DB.Driver {
+	case "mysql":
+		dataSourceName = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+			cfg.DB.User,
+			cfg.DB.Password,
+			cfg.DB.Host,
+			cfg.DB.Port,
+			cfg.DB.Name)
+		db, err = gorm.Open(mysql.Open(dataSourceName), &gorm.Config{Logger: newLogger})
+	case "postgres":
+		dataSourceName = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			cfg.DB.Host,
+			cfg.DB.Port,
+			cfg.DB.User,
+			cfg.DB.Password,
+			cfg.DB.Name)
+		db, err = gorm.Open(postgres.Open(dataSourceName), &gorm.Config{Logger: newLogger})
+	default:
+		panic("unsupported database driver: " + cfg.DB.Driver)
+	}
+
 	if err != nil {
 		panic(err.Error())
 	}
